bc60a5e5629cf2165ef888d803d55ef4d524621c
merndev2 <mern.dev2@ipageums.com>
2026-03-02 14:40:57 +0530

    component

---
+++ component/disclaimmer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Disclaimmer() {
+  return (
+    <div>
+        <h1>Disclaimmer</h1>
+    </div>
+  )
+}
---
+++ component/hero.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+
+export default function hero() {
+  return (
+    <section className="bg-[url(/images/imgi_14_banner-01.jpg)] bg-cover bg-center h-[80vh] flex items-center justify-center">
+      <div className="max-w-7xl mx-auto px-6 py-4 grid grid-cols-2 gap-4 justify-between">
+        <div> left</div>
+        <div className="bg-[#cbd1d2]">
+          <form className="w-full">
+            <h1 className="text-center text-[20px] font-bold">Register EOI</h1>
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="w-full p-2 mb-2 border rounded"
+            />
+            <input
+              type="number"
+              placeholder="Phone Number"
+              className="w-full p-2 mb-2 border rounded"
+            />
+            <input
+              type="number"
+              placeholder="Phone Number"
+              className="w-full p-2 mb-2 border rounded"
+            />
+            <input
+              type="number"
+              placeholder="Phone Number"
+              className="w-full p-2 mb-2 border rounded"
+            />
+            <input
+              type="number"
+              placeholder="Phone Number"
+              className="w-full p-2 mb-2 border rounded"
+            />
+          </form>
+        </div>
+      </div>
+    </section>
+  )
+}
